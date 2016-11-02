b71e6f0072826969c79268440ae1eb2edb0b0860
kdylan <kdylan@qq.com>
2016-11-02 16:45:01 +0800

    完善主题测试模块

--- test/support/support.js
+++ test/support/support.js
@@ -1,10 +1,13 @@
 
-const User = require('../db').user;
+const db = require('../db');
 const supertest = require('supertest');
 const app = require('../../app');
 const config = require('../../config');
 const should = require('should');
 
+const Topic = db.topic;
+const User = db.user;
+const Reply = db.reply;
 
 exports.request = supertest.agent(app.listen(3000));
 
@@ -24,14 +27,7 @@ var createUser = exports.createUser = () => {
   }
 }
 
-var createAndSaveUser = exports.createAndSaveUser = async () => {
-  let user = new User(createUser());
-  return await user.save();
-}
 
-var deleteUser = exports.deleteUser = async (user) => {
-  return await User.remove({ username: user.username });
-}
 
 var getUserCookie = exports.getUserCookie = (user) => {
   return 'dev-user=' + user.username + ';';
@@ -48,6 +44,10 @@ var createTopic = exports.createTopic = () => {
   }
 }
 
+
+/**
+ * 返回错误信息验证
+ */
 var shouldError = exports.shouldError = (msg, done) => {
   return (err, res) => {
     should.not.exist(err);
@@ -57,3 +57,32 @@ var shouldError = exports.shouldError = (msg, done) => {
   }
 }
 
+
+/**
+ * 创建并保存一个topic对象
+ */
+var createAndsaveTopic = exports.createAndSaveTopic = async (user) => {
+  let topic = new Topic(createTopic());
+  topic.author_id = user._id;
+  // 更新用户主题数量
+  await User.update_topic_count(user._id, 1);
+  
+  return await topic.save();
+}
+
+var createAndSaveUser = exports.createAndSaveUser = async () => {
+  let user = new User(createUser());
+  return await user.save();
+}
+
+var removeUser = exports.removeUser = async (user) => {
+  return await User.remove({ username: user.username });
+}
+
+var removeTopic = exports.removeTopic = async (topic) => {
+  return await Topic.remove({title: topic.title});
+}
+
+var removeReply = exports.removeReply = async (reply) => {
+  return await Reply.remove({_id: reply._id});
+}
--- test/topic.test.js
+++ test/topic.test.js
@@ -14,19 +14,22 @@ describe('Topic', () => {
   let user = {};
   let cookie = '';
   let topic = {};
+  let topic2 = {};
 
   before(async () => {
     user = await support.createAndSaveUser();
     cookie = support.getUserCookie(user);
     topic = support.createTopic();
+    topic2 = await support.createAndSaveTopic(user);
   });
 
 
   after(async () => {
-    await support.deleteUser(user);
-    await Topic.remove({
-      title: topic.title
-    });
+    await Promise.all([
+      support.removeUser(user),
+      support.removeTopic(topic),
+      support.removeTopic(topic2)
+    ]);
   });
 
   describe('Create Topic', () => {
@@ -93,34 +96,25 @@ describe('Topic', () => {
           should.not.exist(err);
           res.text.should.containEql(topic.title);
           res.text.should.containEql(topic.content);
+          // 显示作者其它主题
+          res.text.should.containEql(topic2.title);
+          // 显示作者主题数量
+          res.text.should.containEql('<a href="/user/'+ user.username +'/topic">2</a>')
           done();
         })
     });
   });
-
-  
-
-  describe('Check the user data', () => {
-    it('#should the topic_count is right', async (done) => {
-      let userData = await User.findOne({
-        username: user.username 
-      });
-      userData.topic_count.should.equal(1);
-      done();
-    })
-  })
-
-
 })
 
 function notLogin (method, url) {
   return new Promise((resolve, reject) => {
     request[method](url)
-        .expect(200, (err, res) => {
-          if(err) reject(err);
+      .set('Cookie', '')
+      .expect(200, (err, res) => {
+        if(err) reject(err);
 
-          res.text.should.containEql('请先登录!');
-          resolve();
-        });
+        res.text.should.containEql('请先登录!');
+        resolve();
+      });
   })
 }
